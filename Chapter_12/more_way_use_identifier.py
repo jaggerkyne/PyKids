09b352a19fff59c76ba1f31a1eeb73f025163bde
Jagger Kyne <jagger.kyne@gmail.com>
2014-04-19 18:06:19 +0800

    Done up to page 189.

---
+++ Chapter_12/more_way_use_identifier.py
@@ -0,0 +1,13 @@
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2013 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+from tkinter import *
+tk = Tk()
+canvas = Canvas(tk,width=400,height=400)
+canvas.pack()
+canvas.create_polygon(10,10,10,60,50,35)
+canvas.move(1,5,0)
+
+mytriangle = canvas.create_polygon(10,10,10,60,50,35)
+#canvas.move(mytriangle,5,0)
