59fb07b1257ca02af5e99c996e9fdb06c1fc52d6
Jagger Kyne <jagger.kyne@gmail.com>
2014-04-25 18:22:56 +0800

    Up to Page 267

---
+++ Part_02/Chapter_16/puzzles/checkerboard.py
@@ -0,0 +1,47 @@
+# Another code written by Jagger Kyne
+# Copyright 2006 - 2013 Jagger Kyne <jagger.kyne@gmail.com>
+__author__ = 'Jagger Kyne'
+
+from tkinter import *
+import time
+
+class Game:
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.title("Checkerboard")
+        self.tk.resizable(0,0)
+        self.tk.wm_attributes('-topmost',1)
+        self.canvas = Canvas(self.tk,width=500,height=500,highlightthickness=0)
+        self.canvas.pack()
+        self.canvas.update()
+
+        bg_path1 = '/Users/Wilson/Documents/Sites/Python_For_Kids/Part_02/Chapter_16/graphics/background.gif'
+        self.bg = PhotoImage(file=bg_path1)
+
+        w1 = self.bg.width()
+        h1 = self.bg.height()
+
+        draw_background = 0
+
+        for x in range(0,5):
+            for y in range(0,5):
+                if draw_background == 1:
+                    self.canvas.create_image(x*w1,y*h1,image=self.bg,anchor='nw')
+                    draw_background = 0
+                else:
+                    draw_background = 1
+
+        self.sprites = []
+        self.running = True
+
+    def mainloop(self):
+        while 1:
+            if self.running == True:
+                for sprite in self.sprites:
+                    sprite.move()
+            self.tk.update_idletasks()
+            self.tk.update()
+            time.sleep(0.01)
+
+game = Game()
+game.mainloop()
